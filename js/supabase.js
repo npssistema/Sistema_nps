25e31689cf299751d441416737fb44133d3658d1
npsistema <npssistema@gmail.com.br>
2026-04-27 22:02:43 -0300

    Mudança

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://maifzrzarjbnjqehfajv.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_tiQMppNIHYHFFHLCtJ2-8w_VmMPNSBc";
+const SUPABASE_URL = "https://maifzrzarjbnjqehfajv.supabase.co";
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
