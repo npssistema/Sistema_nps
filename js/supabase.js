cbe0051afeb8212d56a61fb5445598fd300f63cc
npsistema <npssistema@gmail.com.br>
2026-04-27 22:08:54 -0300

    tentativa 2

--- js/supabase.js
+++ js/supabase.js
@@ -1,3 +1,5 @@
+console.log("SUPABASE JS CARREGOU");
+
 const SUPABASE_URL = "https://maifzrzarjbnjqehfajv.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_tiQMppNIHYHFFHLCtJ2-8w_VmMPNSBc";
 
