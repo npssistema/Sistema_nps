965b51641c338d07a7c99da61408a00b1338e2ec
npssistema <npssistema@gmail.com>
2026-06-16 12:15:05 -0300

    Update graficos.js

--- js/graficos.js
+++ js/graficos.js
@@ -629,7 +629,7 @@ async function carregarGraficos() {
     try {
         esconderStatus();
         atualizarUrlComFiltros();
-        atualLinksHistorico();
+        atualizarLinksHistorico(); // CORRIGIDO AQUI!
 
         const avaliacoes = await buscarAvaliacoes();
 
